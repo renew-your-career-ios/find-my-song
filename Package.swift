9ba1434bd6969896a80ffd6635864001f5028ece
Fernando Marques <fernandohenriqueom@gmail.com>
2025-04-01 21:45:12 -0300

    criando storyboard

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Dangerfile",
-    platforms: [.macOS(.v14)],
+    platforms: [.macOS(.v13)],
     products: [.library(name: "DangerDeps", type: .dynamic, targets: ["Dangerfile"])],
     dependencies: [
         .package(url: "https://github.com/danger/swift.git", exact: "3.21.1"),
