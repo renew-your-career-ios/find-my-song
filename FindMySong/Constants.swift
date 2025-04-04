21dea2f48311ea19b36e408f9cf54cba744403f3
Henrique Cisi <henrique.cisi@accenture.com>
2025-04-04 15:57:58 -0300

    feat: add constants file

---
+++ FindMySong/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  FindMySong
+//
+//  Created by henrique.cisi on 04/04/25.
+//
+
+struct Constants {
+    
+    struct LoginPage {
+        static let title = "find my Song"
+        static let songRange = "Song"
+        static let songColor = "SongColor"
+    }
+}
