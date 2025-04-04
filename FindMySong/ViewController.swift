8e2819b80c84c0208f1a74928a60b14883ceaa9e
Henrique Cisi <henrique.cisi@accenture.com>
2025-04-04 18:06:47 -0300

    feat: adds title view

--- FindMySong/ViewController.swift
+++ FindMySong/ViewController.swift
@@ -8,15 +8,64 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupView()
+        setupStackView()
     }
-
-    func setupView() {
-        view.backgroundColor = .systemRed
+    
+    func setupStackView() {
+        view.backgroundColor = .white
+        
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        
+        let titleView = setupTitleView()
+        
+        stackView.addArrangedSubview(titleView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
     }
+    
+    func setupTitleView() -> UIView {
+        let titleView = UIView()
+        
+        let titleLabel = UILabel()
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.font = UIFont.systemFont(ofSize: 32, weight: .regular)
+        
+        let title = Constants.LoginPage.title
+        
+        let attributedText = NSMutableAttributedString(string: title, attributes: [
+            .font: UIFont.boldSystemFont(ofSize: 32)
+        ])
 
+        let songRange = (title as NSString).range(of: Constants.LoginPage.songRange)
+
+        attributedText.addAttribute(.font, value: UIFont.systemFont(ofSize: 32), range: songRange)
+        attributedText.addAttribute(.foregroundColor, value: UIColor(named: Constants.LoginPage.songColor)!, range: songRange)
+        
+        titleLabel.attributedText = attributedText
+        
+        titleView.addSubview(titleLabel)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: titleView.centerXAnchor),
+            titleLabel.centerYAnchor.constraint(equalTo: titleView.centerYAnchor, constant: -150)
+        ])
+        
+        return titleView
+    }
 }
 
