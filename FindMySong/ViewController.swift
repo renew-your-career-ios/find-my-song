2848d5cd7adf3f643819e83182dcf2d6e70436f8
Watson Matos <watsontrovati@gmail.com>
2025-04-07 16:30:40 -0300

    feat: Refactoring login screen - issue #11

--- FindMySong/ViewController.swift
+++ FindMySong/ViewController.swift
@@ -1,3 +1,11 @@
+//
+//  ViewController.swift
+//  FindMySong
+//
+//  Created by saulo.santos.freire on 26/03/25.
+//  Updated by watson.matos on 07/04/25.
+//
+
 import UIKit
 
 class ViewController: UIViewController {
